65f306f1dadb9120e0b9b899722b2c53ddf61ca1
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-07-06 04:44:52 -0700

    Se agrego la funcionalidad de agregar numeros con sus validaciones y la funcionalidade de delete

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -18,8 +18,50 @@ const CalculadoraScreen = () => {
 
 
     const armarNumero = (numeroTexto: string) => {
-        setNumero(numero + numeroTexto)
+
+        //No aceptar doble punto
+        if (numero[numero.length - 1] === '.' && numeroTexto === '.') return;
+
+        if (numero.startsWith('0') || numero.startsWith('-0')) {
+            // Punto decimal
+            if (numeroTexto === '.') {
+                setNumero(numero + numeroTexto);
+
+                // Evaluar si es otro cero y hay un punto
+            } else if (numeroTexto === '0' && numero.includes('.')) {
+                setNumero(numero + numeroTexto)
+                //Evaluar si es difertente de cero y no tiene un punto
+            } else if (numeroTexto !== '0' && !numero.includes('.')) {
+                setNumero(numeroTexto)
+
+            } else if (numeroTexto === '0' && !numero.includes('.')) {
+                setNumero(numero)
+            } else {
+                setNumero(numero + numeroTexto)
+            }
+
+        } else {
+            setNumero(numero + numeroTexto)
+        }
+
     }
+
+    const positivoNegativo = () => {
+        if (numero.includes('-')) {
+            setNumero(numero.replace('-', ''));
+        } else {
+            setNumero('-' + numero)
+        }
+    }
+
+    const btnDelete = () => {
+        if (numero.length === 1 || numero[numero.length - 2] === '-') {
+            setNumero('0')
+        } else {
+            setNumero(numero.slice(0, -1))
+        }
+    }
+
 
     return (
         <View style={styles.calculadoraContainer}>
@@ -28,32 +70,32 @@ const CalculadoraScreen = () => {
             {/* Fila de botones */}
             <View style={styles.fila}>
                 <BotonCalc texto='C' color='#9B9B9B' action={limpiar} />
-                <BotonCalc texto='+/-' color='#9B9B9B' action={limpiar}/>
-                <BotonCalc texto='DEL' color='#9B9B9B' action={limpiar}/>
-                <BotonCalc texto='/' color='#FF9427' action={limpiar}/>
+                <BotonCalc texto='+/-' color='#9B9B9B' action={positivoNegativo} />
+                <BotonCalc texto='DEL' color='#9B9B9B' action={btnDelete} />
+                <BotonCalc texto='/' color='#FF9427' action={limpiar} />
             </View>
             <View style={styles.fila}>
                 <BotonCalc texto='7' action={armarNumero} />
-                <BotonCalc texto='8' action={armarNumero}/>
-                <BotonCalc texto='9' action={armarNumero}/>
-                <BotonCalc texto='X' color='#FF9427' action={limpiar}/>
+                <BotonCalc texto='8' action={armarNumero} />
+                <BotonCalc texto='9' action={armarNumero} />
+                <BotonCalc texto='X' color='#FF9427' action={limpiar} />
             </View>
             <View style={styles.fila}>
-                <BotonCalc texto='4' action={armarNumero}/>
-                <BotonCalc texto='5' action={armarNumero}/>
-                <BotonCalc texto='6' action={armarNumero}/>
-                <BotonCalc texto='-' color='#FF9427' action={limpiar}/>
+                <BotonCalc texto='4' action={armarNumero} />
+                <BotonCalc texto='5' action={armarNumero} />
+                <BotonCalc texto='6' action={armarNumero} />
+                <BotonCalc texto='-' color='#FF9427' action={limpiar} />
             </View>
             <View style={styles.fila}>
-                <BotonCalc texto='1' action={armarNumero}/>
-                <BotonCalc texto='2' action={armarNumero}/>
-                <BotonCalc texto='3' action={armarNumero}/>
+                <BotonCalc texto='1' action={armarNumero} />
+                <BotonCalc texto='2' action={armarNumero} />
+                <BotonCalc texto='3' action={armarNumero} />
                 <BotonCalc texto='+' color='#FF9427' action={limpiar} />
             </View>
             <View style={styles.fila}>
                 <BotonCalc texto='0' ancho action={armarNumero} />
-                <BotonCalc texto='+/-' action={limpiar}/>
-                <BotonCalc texto='=' color='#FF9427' action={limpiar}/>
+                <BotonCalc texto='.' action={armarNumero} />
+                <BotonCalc texto='=' color='#FF9427' action={limpiar} />
             </View>
         </View>
     )
